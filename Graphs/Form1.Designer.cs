35391017a7b1082bf2abdf2a2c29d3d7f5b586bd
Inblinder <52979050+Inblinder@users.noreply.github.com>
2022-08-06 23:44:57 +0200

    Start

---
+++ Graphs/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace Graphs
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.matrix = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fixedUpdate = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+            this.splitContainer.Panel1.SuspendLayout();
+            this.splitContainer.Panel2.SuspendLayout();
+            this.splitContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer
+            // 
+            this.splitContainer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(252)))));
+            this.splitContainer.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer.IsSplitterFixed = true;
+            this.splitContainer.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer.Name = "splitContainer";
+            // 
+            // splitContainer.Panel1
+            // 
+            this.splitContainer.Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(28)))), ((int)(((byte)(36)))));
+            this.splitContainer.Panel1.Controls.Add(this.label2);
+            this.splitContainer.Panel1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.splitContainer.Panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.CreateVertex);
+            // 
+            // splitContainer.Panel2
+            // 
+            this.splitContainer.Panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(252)))));
+            this.splitContainer.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer.Panel2.Controls.Add(this.matrix);
+            this.splitContainer.Panel2.Controls.Add(this.label1);
+            this.splitContainer.Panel2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.splitContainer.Size = new System.Drawing.Size(882, 503);
+            this.splitContainer.SplitterDistance = 550;
+            this.splitContainer.TabIndex = 0;
+            this.splitContainer.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(252)))));
+            this.label2.Location = new System.Drawing.Point(13, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "click to create vertex";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Graphs.Properties.Resources.dot;
+            this.pictureBox1.Location = new System.Drawing.Point(246, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(70, 70);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // matrix
+            // 
+            this.matrix.AutoSize = true;
+            this.matrix.Location = new System.Drawing.Point(18, 258);
+            this.matrix.Name = "matrix";
+            this.matrix.Size = new System.Drawing.Size(28, 20);
+            this.matrix.TabIndex = 1;
+            this.matrix.Text = "[...]";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(18, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "G = (V, E)";
+            // 
+            // fixedUpdate
+            // 
+            this.fixedUpdate.Interval = 20;
+            this.fixedUpdate.Tick += new System.EventHandler(this.fixedUpdate_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(882, 503);
+            this.Controls.Add(this.splitContainer);
+            this.MinimumSize = new System.Drawing.Size(500, 300);
+            this.Name = "Form1";
+            this.Text = "Graphs";
+            this.splitContainer.Panel1.ResumeLayout(false);
+            this.splitContainer.Panel1.PerformLayout();
+            this.splitContainer.Panel2.ResumeLayout(false);
+            this.splitContainer.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+            this.splitContainer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private void Panel1_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label matrix;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer fixedUpdate;
+    }
+}
+
---
+++ Graphs/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graphs
+{
+    public partial class Form1 : Form
+    {
+        List<Vertex> vertices = new List<Vertex>();
+        List<Edge> edges = new List<Edge>();
+        int[,] adjMatrix = new int[8, 8];
+        int origin;
+        int termin;
+        bool drag = false;
+        Pen penPrevState;
+        Point prevMousePos;
+        Point pos;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void CreateVertex(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                pos = e.Location;
+
+                if (drag)
+                {
+                    termin = vertices.Count + 1;
+                    edges.Add(new Edge(origin, termin, edges.Count + 1));
+                    AdjustMatrix(origin, termin, edges.Count);
+                    origin = termin;
+                }
+                else
+                {
+                    origin = vertices.Count + 1;
+                    drag = true;
+                }
+
+                vertices.Add(new Vertex(pos, vertices.Count + 1, splitContainer.Panel1));
+                PrintMatrix(vertices.Count);
+
+                Bitmap bmpPrevState = new Bitmap(splitContainer.Panel1.Width, splitContainer.Panel1.Height);
+                Graphics.FromImage(bmpPrevState).CopyFromScreen(PointToScreen(splitContainer.Panel1.Location), new Point(0, 0), splitContainer.Panel1.Size);
+                penPrevState = new Pen(new TextureBrush(bmpPrevState), 12);
+                penPrevState.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+
+                fixedUpdate.Start();
+            }
+            else
+            {
+                fixedUpdate.Stop();
+                Graphics g = splitContainer.Panel1.CreateGraphics();
+                g.DrawLine(penPrevState, pos, prevMousePos);
+                drag = false;
+            } 
+        }
+
+        public void AdjustMatrix(int v_1, int v_2, int edge_count)
+        {
+            int size = adjMatrix.GetLength(0);
+            if (vertices.Count >= size)
+            {
+                adjMatrix = new int[size * 2, size * 2];
+                for (int i = 0; i < edge_count; i++) { adjMatrix[edges[i].v_1 - 1, edges[i].v_2 - 1] = 1; adjMatrix[edges[i].v_2 - 1, edges[i].v_1 - 1] = 1; }
+            }
+            
+            adjMatrix[v_1 - 1, v_2 - 1] = 1; adjMatrix[v_2 - 1, v_1 - 1] = 1;
+        }
+
+        public void PrintMatrix(int vertex_count)
+        {
+            string m = "";
+            for (int i = 0; i < vertex_count; i++)
+                for (int j = 0; j < vertex_count; j++)
+                    if (j < vertex_count - 1) m += $"{adjMatrix[i, j]}  ";
+                    else m += $"{adjMatrix[i, j]}\n";
+            matrix.Text = m;
+        }
+
+        private void fixedUpdate_Tick(object sender, EventArgs e)
+        {
+            if (prevMousePos != splitContainer.Panel1.PointToClient(MousePosition))
+            {
+                Graphics g = splitContainer.Panel1.CreateGraphics();
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+                g.DrawLine(penPrevState, pos, new Point(prevMousePos.X, prevMousePos.Y));
+                prevMousePos = splitContainer.Panel1.PointToClient(MousePosition);
+                Pen whtPen = new Pen(Color.White, 10);
+                whtPen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+                g.DrawLine(whtPen, pos, prevMousePos);
+            }
+        }
+    }
+
+    public class Vertex
+    {
+        public int num;
+        public Point pos;
+        PictureBox dotka;
+         
+        public Vertex(Point pos, int num, Panel panel_one)
+        {
+            this.pos = pos;
+            this.num = num;
+            
+            dotka = new PictureBox();
+            dotka.Size = new Size(30, 30);
+            dotka.Location = new Point(pos.X - 15, pos.Y - 15);
+            dotka.SizeMode = PictureBoxSizeMode.StretchImage;
+            dotka.Image = Properties.Resources.dot;
+            panel_one.Controls.Add(dotka);
+        }
+    }
+
+    public class Edge
+    {
+        private int num;
+        public int v_1;
+        public int v_2;
+
+        public Edge(int v_1, int v_2, int num)
+        {
+            this.v_1 = v_1;
+            this.v_2 = v_2;
+            this.num = num;
+        }
+    }
+}
